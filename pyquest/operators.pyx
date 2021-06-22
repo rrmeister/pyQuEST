99f860a8c772192910d4c7b049d2ebdecfb7639f
Richard Meister <richardm.tug@gmail.com>
2021-06-22 19:55:16 +0100

    Implement PhaseFunc operator
    Adding support for the new QuEST internal functions
    apply*PhaseFunc* implementing specific diagonal operators via a
    new operator class PhaseFunc.

--- pyquest/operators.pyx
+++ pyquest/operators.pyx
@@ -20,11 +20,13 @@ Classes:
         A sum of ´´PauliProduct´´s.
 """
 
-import numpy as np
+import enum
+import logging
+from warnings import warn
 import cython
+import numpy as np
 import pyquest
 from pyquest.quest_error import QuESTError
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -453,3 +455,358 @@ cdef class DiagonalOperator(GlobalOperator):
     def __cinit__(self, diag_elements, num_qubits):
         self.TYPE = OP_TYPES.OP_DIAGONAL
         raise NotImplementedError("DiagonalOperator not yet implemented.")
+
+
+class _BitEncoding(enum.IntEnum):
+    UNSIGNED = quest.bitEncoding.UNSIGNED
+    TWOS_COMPLEMENT = quest.bitEncoding.TWOS_COMPLEMENT
+
+
+class _PhaseFuncType(enum.IntEnum):
+    # Careful when adding new functions to this enum, the PhaseFunc
+    # constructor and __repr__ rely on the "SCALED" and "INVERSE" naming
+    # conventions to determine the correct structure of parameters to
+    # pass to QuEST.
+    NORM = quest.phaseFunc.NORM
+    SCALED_NORM = quest.phaseFunc.SCALED_NORM
+    INVERSE_NORM = quest.phaseFunc.INVERSE_NORM
+    SCALED_INVERSE_NORM = quest.phaseFunc.SCALED_INVERSE_NORM
+    PRODUCT = quest.phaseFunc.PRODUCT
+    SCALED_PRODUCT = quest.phaseFunc.SCALED_PRODUCT
+    INVERSE_PRODUCT = quest.phaseFunc.INVERSE_PRODUCT
+    SCALED_INVERSE_PRODUCT = quest.phaseFunc.SCALED_INVERSE_PRODUCT
+    DISTANCE = quest.phaseFunc.DISTANCE
+    SCALED_DISTANCE = quest.phaseFunc.SCALED_DISTANCE
+    INVERSE_DISTANCE = quest.phaseFunc.INVERSE_DISTANCE
+    SCALED_INVERSE_DISTANCE = quest.phaseFunc.SCALED_INVERSE_DISTANCE
+    EXPONENTIAL_POLYNOMIAL = enum.auto()
+
+
+# Extension types do not allow nested classes, so they are defined as
+# "private" in the module scope and referenced in the PhaseFunc
+# extension type. The __str__ and __repr__ methods of IntEnum use the
+# __name__ attribute for printing, so remove the underscore here for
+# nicer and less confusing prints.
+_BitEncoding.__name__ = "BitEncoding"
+_PhaseFuncType.__name__ = "FuncType"
+
+
+cdef class PhaseFunc(GlobalOperator):
+
+    BitEncoding = _BitEncoding
+    FuncType = _PhaseFuncType
+
+    # Maximum number of parameters for any named function.
+    _MAX_NUM_PARAMETERS = 2
+
+    def __cinit__(
+            self, targets, func_type='exponential_polynomial',
+            bit_encoding='unsigned', overrides=None, **kwargs):
+        """Create a new phase function with specific parameters.
+
+        Args:
+            targets (iterable[int] | iterable[iterable[int]]): Qubits
+                the phase function should be applied to. If given as
+                iterable of `int`s, specifies the qubits which,
+                interpreted as a binary number, serve as the input to
+                a single-variable phase function. The first qubit in the
+                iterable is the least significant bit in its binary
+                interpretation. A state with qubit 0 in state 0 and
+                qubit 2 in state 1 is interpreted as a decimal 4 if
+                targets=(0, 1, 2), but as a decimal 1 if
+                targets=(2, 1, 0) (when using unsigned binary
+                representation). The qubit indices do not need to be
+                in strictly increasing or decreasing order.
+
+                For multi-variable functions, `targets` must be a
+                nested iterable, where each element is an iterable of
+                integers. Each of the elements in `targets` then
+                represents a sub-register in the same fashion as
+                described above, and each such sub-register serves as
+                one variable in a multi-variable function. How exactly
+                they are used depends on the function type.
+            func_type (PhaseFunc.FuncType | str): The type of phase
+                function to apply. Must be either directly from enum
+                PhaseFunc.FuncType or a string matching one of the
+                elements therein. Defaults to 'exponential_polynomial'.
+            bit_encoding (PhaseFunc.BitEncoding | str): The encoding
+                by which the binary representation in the registers
+                is interpreted. Must be either directly from enum
+                PhaseFunc.BitEncoding or a string matching one of the
+                elements therein. Defaults to 'unsigned'.
+            overrides (dict[int: float] | dict[tuple[int]: float]):
+                Manually replaces the function output for certain
+                input values. For single-valued functions, the keys
+                can be integers and directly specify for which inputs
+                the function output should be replaced. The value of
+                each dictionary element gives the phase to be applied
+                to the matching input. Passing `overrides={4: 3.14}`
+                would multiply the coefficient of the basis state
+                corresponding to decimal 4 (in the chosen binary
+                encoding) with exp(3.14j).
+
+                For multi-valued functions, the keys of the dictionary
+                must be tuples, where entry `k` of the tuple
+                corresponds to the state the `k`th sub-register must
+                be in to be overridden. When working with two
+                sub-registers, `overrides={(3, 5): 3.14}` would effect
+                a phase of exp(3.14j) on a state where the first
+                sub-register is in a state corresponding to decimal 3,
+                and the state of the second sub-register corresponds to
+                decimal 5 simultaneously. The order of the sub-registers
+                is defined by the order they appear in the `targets`
+                argument. Defaults to None.
+
+        Keyword Args:
+            terms (iterable[iterable[int]]): The function type
+                'exponential_polynomial' requires the polynomial to be
+                passed to this keyword argument. The exponential
+                polynomial is a sum of terms, and, each term is of the
+                form `c * (x_r)**k`. This is represented by an iterable
+                of length 3 of the form (c, r, k). The first element is
+                the coefficient, the second element is the index of the
+                sub-register to be used, and the third element is the
+                exponent. `terms` is an iterable, where each element is
+                such a term.
+
+                The exponential polynomial
+                    f(r) = r0**2 + 2 * r1 + 4 * r1**(-1) - 3.1 * r2**0.5
+                acting on three sub-registers would be specified by
+                passing
+                    `terms=[(1, 0, 2), (2, 1, 1),
+                            (4, 1, -1), (-3.1, 2, .5)]`
+                to the constructor.
+            scaling (float): Function types containing `SCALED` in their
+                name require this costant factor by which the function
+                values are multiplied. Not allowed for any other
+                function types.
+            divergence_override (float): Allows automatic overriding of
+                function values at divergent points. Only available for
+                function types containing `INVERSE` in their name, in
+                which it replaces occurances where the denominator is
+                zero with the specified value `divergence_override`,
+                effectively multiplying their coefficients with
+                exp(1j * divergence_override). Defaults to 0.
+        """
+        cdef int k, m, n
+        cdef int num_qubits_in_regs, qubit_counter
+        cdef int num_terms, term_counter
+
+        if not isinstance(func_type, PhaseFunc.FuncType):
+            try:
+                func_type = PhaseFunc.FuncType[func_type.upper()]
+            except KeyError:
+                raise ValueError(f'"{func_type}" is not a valid function type.') from None
+        self._phase_func_type = func_type
+        if func_type is PhaseFunc.FuncType.EXPONENTIAL_POLYNOMIAL:
+            self._is_poly = 1  # For quicker check in apply_to().
+
+        if not isinstance(bit_encoding, PhaseFunc.BitEncoding):
+            try:
+                bit_encoding = PhaseFunc.BitEncoding[bit_encoding.upper()]
+            except KeyError:
+                raise ValueError(f'"{bit_encoding}" is not a valid bit encoding.') from None
+        self._bit_encoding = bit_encoding
+
+        # Pack single register spec into a tuple for consistent parsing.
+        if isinstance(targets[0], int):
+            targets = (targets,)
+        self._num_regs = len(targets)
+        num_qubits_in_regs = sum(len(sub_reg) for sub_reg in targets)
+        self._num_qubits_per_reg = <int*>malloc(
+            self._num_regs * sizeof(self._num_qubits_per_reg[0]))
+        self._qubits_in_regs = <int*>malloc(
+            num_qubits_in_regs * sizeof(self._qubits_in_regs[0]))
+        qubit_counter = 0
+        for k in range(self._num_regs):
+            self._num_qubits_per_reg[k] = len(targets[k])
+            for m in range(self._num_qubits_per_reg[k]):
+                self._qubits_in_regs[qubit_counter] = targets[k][m]
+                qubit_counter += 1
+
+        if overrides is None:
+            self._num_overrides = 0
+        else:
+            self._num_overrides = len(overrides)
+            self._override_inds = <long long int*>malloc(
+                self._num_regs * self._num_overrides
+                * sizeof(self._override_inds[0]))
+            self._override_phases = <qreal*>malloc(
+                self._num_overrides * sizeof(self._override_phases[0]))
+            # The extraction of the override indices can lead to an
+            # illegal object state if the keys in the 'overrides' dict
+            # are of different lengths, so explicitly check that first.
+            override_lens = set(1 if isinstance(override, int)
+                                else len(override)
+                                for override in overrides)
+            if (len(override_lens) != 1
+                    or override_lens.pop() != self._num_regs):
+                raise ValueError("All keys of the 'override' dict must "
+                                 "have a length equal to the number of "
+                                 "sub-registers.")
+            k = 0
+            m = 0
+            for cur_override_ind in overrides:
+                if isinstance(cur_override_ind, int):
+                    self._override_inds[m] = cur_override_ind
+                    m += 1
+                else:
+                    for cur_override_sub_ind in cur_override_ind:
+                        self._override_inds[m] = cur_override_sub_ind
+                        m += 1
+                self._override_phases[k] = overrides[cur_override_ind]
+                k += 1
+
+        if func_type is PhaseFunc.FuncType.EXPONENTIAL_POLYNOMIAL:
+            if 'terms' not in kwargs:
+                raise TypeError("Exponential polynomial needs "
+                                "argument 'terms'.")
+            terms = kwargs['terms']
+            if not all([0 <= term[1] < self._num_regs
+                       for term in terms]):
+                raise ValueError("All register indices must be >= 0 "
+                                 "and < number of sub-registers.")
+            self._num_terms_per_reg = <int*>malloc(
+                self._num_regs * sizeof(self._num_terms_per_reg[0]))
+            num_terms = len(terms)
+            self._exponents=<qreal*>malloc(sizeof(self._exponents[0]))
+            self._exponents = <qreal*>malloc(
+                num_terms * sizeof(self._exponents[0]))
+            self._coeffs = <qreal*>malloc(
+                num_terms * sizeof(self._coeffs[0]))
+            term_counter = 0
+            for k in range(self._num_regs):
+                subreg_terms = [term for term in terms if term[1] == k]
+                self._num_terms_per_reg[k] = len(subreg_terms)
+                for term in subreg_terms:
+                    self._coeffs[term_counter] = term[0]
+                    self._exponents[term_counter] = term[2]
+                    term_counter += 1
+
+        # Since we control the _PhaseFuncType enum and must port
+        # additional functions manually, we can rely on functions
+        # that support scaling to contain 'SCALED', and potentially
+        # diverging functions to contain 'INVERSE'.
+        if 'scaling' in kwargs and 'SCALED' not in func_type.name:
+            raise TypeError("'scaling' argument is only allowed for "
+                            "scalable functions.")
+
+        if 'SCALED' in func_type.name and 'scaling' not in kwargs:
+            raise TypeError("Scaled function needs 'scaling' "
+                            "keyword argument.")
+
+        if 'divergence_override' in kwargs and 'INVERSE' not in func_type.name:
+            raise TypeError(
+                "'divergence_override' only available for predefined "
+                "'INVERSE' functions. Specify manually via 'overrides' "
+                "for exponential polynomials.")
+
+        if 'INVERSE' in func_type.name and 'divergence_override' not in kwargs:
+            kwargs['divergence_override'] = 0
+
+        # Just allocate as much memory as we can possibly need,
+        # this is still negligible memory waste.
+        self._parameters = <qreal*>malloc(
+            PhaseFunc._MAX_NUM_PARAMETERS * sizeof(self._parameters[0]))
+        self._num_parameters = 0
+
+        if 'SCALED' in func_type.name:
+            self._parameters[self._num_parameters] = kwargs['scaling']
+            self._num_parameters += 1
+
+        if 'INVERSE' in func_type.name:
+            self._parameters[self._num_parameters] = kwargs['divergence_override']
+            self._num_parameters += 1
+
+    def __dealloc__(self):
+        free(self._override_inds)
+        free(self._override_phases)
+        free(self._parameters)
+        free(self._num_qubits_per_reg)
+        free(self._qubits_in_regs)
+        free(self._num_terms_per_reg)
+        free(self._exponents)
+        free(self._coeffs)
+
+    def __repr__(self):
+        res = type(self).__name__ + "("
+        res += f"targets={str(self.targets)}"
+        res += f', func_type="{PhaseFunc.FuncType(self._phase_func_type).name.lower()}"'
+        res += f', bit_encoding="{PhaseFunc.BitEncoding(self._bit_encoding).name.lower()}"'
+        if self._num_overrides:
+            res += ", overrides=" + str(self.overrides)
+        if self._num_terms_per_reg:
+            res += f", terms={self.terms}"
+        if "SCALED" in PhaseFunc.FuncType(self._phase_func_type).name:
+            res += f", scaling={self._parameters[0]}"
+        if "INVERSE" in PhaseFunc.FuncType(self._phase_func_type).name:
+            res += f", divergence_override={self._parameters[self._num_parameters - 1]}"
+        return res + ")"
+
+    @property
+    def overrides(self):
+        if self._num_regs > 1:
+            ind_iterator = iter(self._override_inds[k]
+                                for k in range(self._num_regs * self._num_overrides))
+            return {tuple(next(ind_iterator)
+                          for _ in range(self._num_regs)):
+                    self._override_phases[k]
+                    for k in range(self._num_overrides)}
+        else:
+            return {self._override_inds[k]: self._override_phases[k]
+                    for k in range(self._num_overrides)}
+
+    @property
+    def targets(self):
+        num_reg_qubits = sum(self._num_qubits_per_reg[k]
+                             for k in range(self._num_regs))
+        reg_qubits = iter(self._qubits_in_regs[k]
+                          for k in range(num_reg_qubits))
+        if self._num_regs == 1:
+            return tuple(reg_qubits)
+        return tuple(tuple(next(reg_qubits) for m in range(self._num_qubits_per_reg[k]))
+                     for k in range(self._num_regs))
+
+    @property
+    def terms(self):
+        num_terms = sum(self._num_terms_per_reg[k] for k in range(self._num_regs))
+        coeffs = iter(self._coeffs[k] for k in range(num_terms))
+        exponents = iter(self._exponents[k] for k in range(num_terms))
+        return tuple((next(coeffs), k, next(exponents))
+                     for k in range(self._num_regs)
+                     for _ in range(self._num_terms_per_reg[k]))
+
+    @property
+    def scaling(self):
+        if 'SCALING' not in PhaseFunc.FuncType(self._phase_func_type):
+            return None
+        return self._parameters[0]
+
+    @property
+    def divergence_override(self):
+        if 'INVERSE' not in PhaseFunc.FuncType(self._phase_func_type):
+            return None
+        return self._parameters[self._num_parameters - 1]
+
+    cdef int apply_to(self, Qureg c_register) except -1:
+        pass
+        if self._is_poly:
+            if self._num_regs == 0:
+                quest.applyPhaseFuncOverrides(
+                    c_register, self._qubits_in_regs, self._num_qubits_per_reg[0],
+                    self._bit_encoding, self._coeffs, self._exponents,
+                    self._num_terms, self._override_inds,
+                    self._override_phases, self._num_overrides)
+            else:
+                quest.applyMultiVarPhaseFuncOverrides(
+                    c_register, self._qubits_in_regs, self._num_qubits_per_reg,
+                    self._num_regs, self._bit_encoding, self._coeffs,
+                    self._exponents, self._num_terms_per_reg,
+                    self._override_inds, self._override_phases,
+                    self._num_overrides)
+        else:
+            quest.applyParamNamedPhaseFuncOverrides(
+                c_register, self._qubits_in_regs, self._num_qubits_per_reg,
+                self._num_regs, self._bit_encoding, self._phase_func_type,
+                self._parameters, self._num_parameters, self._override_inds,
+                self._override_phases, self._num_overrides)
